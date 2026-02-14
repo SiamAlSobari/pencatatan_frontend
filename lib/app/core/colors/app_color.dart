d0d069f0ffa16bb9d7474f19b7a544c0cd1cf562
SiamAlSobari <siamqueen52@gmail.com>
2026-02-15 00:27:11 +0700

    feat: enhance splash view with new layout and add AppColor class for color management

---
+++ lib/app/core/colors/app_color.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/widgets.dart';
+
+class AppColor {
+  static const primary = Color(0xFF1142D4);
+}
--- lib/app/modules/splash/views/splash_view.dart
+++ lib/app/modules/splash/views/splash_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:mobile/app/core/colors/app_color.dart';
 
 import '../controllers/splash_controller.dart';
 
@@ -9,15 +10,52 @@ class SplashView extends GetView<SplashController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('SplashView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'SplashView is working',
-          style: TextStyle(fontSize: 20),
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Column(
+            children: [
+              Container(
+                height: 100,
+                width: 100,
+                decoration: BoxDecoration(
+                  color: AppColor.primary,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Center(
+                  child: Image.asset('assets/images/icon.png',
+                      height: 60,
+                      errorBuilder: (context, error, stackTrace) => const Icon(
+                            Icons.error,
+                            color: Colors.white,
+                          )),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Finance',
+                    style: Goo(
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold,
+                        color: AppColor.primary),
+                  ),
+                  Text(
+                    'Flow',
+                    style: TextStyle(
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.primary.withAlpha(150),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ))
+        ],
       ),
     );
   }
