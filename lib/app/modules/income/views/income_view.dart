4f0ec5451cf6c995beb309bb6b8b425d599dbb1f
SiamAlSobari <siamqueen52@gmail.com>
2026-02-19 15:01:16 +0700

    feat: enhance IncomeView UI; add category selector and improve layout with spacing adjustments

--- lib/app/modules/income/views/income_view.dart
+++ lib/app/modules/income/views/income_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:mobile/app/core/colors/app_color.dart';
 import 'package:mobile/app/core/utils/currency_format.dart';
 
 import '../controllers/income_controller.dart';
@@ -34,9 +35,12 @@ class IncomeView extends GetView<IncomeController> {
         padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
         child: SingleChildScrollView(
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 20),
               _buildTransactionNominalInput(),
+              const SizedBox(height: 30),
+              _buildCategorySelector(),
             ],
           ),
         ),
@@ -44,46 +48,117 @@ class IncomeView extends GetView<IncomeController> {
     );
   }
 
+  Widget _buildCategorySelector() {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Icon(
+              Icons.category,
+              color: Colors.green,
+            ),
+            const SizedBox(width: 8),
+            Text(
+              'Pilih Kategori',
+              style: GoogleFonts.manrope(
+                fontSize: 16,
+                color: AppColor.text,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        Obx(() {
+          final selectedId = controller.selectedCategoryId.value;
+
+          return GridView.builder(
+            itemCount: 6,
+            shrinkWrap: true, // penting untuk GridView dalam Column
+            physics:
+                const NeverScrollableScrollPhysics(), // agar tidak bisa di-scroll
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, // 3 kolom
+              crossAxisSpacing: 12,
+              mainAxisSpacing: 12,
+              childAspectRatio: 2.6, // lebar > tinggi (chip look)
+            ),
+            itemBuilder: (context, index) {
+              final isActive =
+                  selectedId == index.toString();
+              return GestureDetector(
+                onTap: () {
+                  controller.selectedCategoryId.value = index.toString();
+                },
+                child: Container(
+                  width: 80,
+                  height: 80,
+                  decoration: BoxDecoration(
+                    color: isActive ? Colors.green : Colors.grey.withAlpha(100),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Center(
+                    child: Text(
+                      'Kategori ${index + 1}',
+                      style: GoogleFonts.manrope(
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          );
+        })
+      ],
+    );
+  }
+
   Widget _buildTransactionNominalInput() {
     return Center(
-        child: Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Text(
-          'Nominal Pemasukan',
-          style: GoogleFonts.manrope(
-            fontSize: 14,
-            color: Color(0xFF64748B),
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        const SizedBox(height: 8),
-        TextField(
-          controller: controller.amountInput,
-          decoration: InputDecoration(
-            enabledBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.grey.withAlpha(80),
-                width: 1,
-              ),
-            ),
-            focusedBorder: UnderlineInputBorder(
-              borderSide: BorderSide(
-                color: Colors.green.withAlpha(120),
-                width: 1.2,
-              ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Nominal Pemasukan',
+            style: GoogleFonts.manrope(
+              fontSize: 14,
+              color: Color(0xFF64748B),
+              fontWeight: FontWeight.bold,
             ),
           ),
-          keyboardType: TextInputType.number,
-          textAlign: TextAlign.center,
-          inputFormatters: [CurrencyInputFormatter()],
-          style: GoogleFonts.manrope(
-            fontSize: 36,
-            color: Colors.green,
-            fontWeight: FontWeight.bold,
-          ),
-        )
-      ],
-    ));
+          const SizedBox(height: 8),
+          TextField(
+            controller: controller.amountInput,
+            decoration: InputDecoration(
+              enabledBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.grey.withAlpha(80),
+                  width: 1,
+                ),
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.green.withAlpha(120),
+                  width: 1.2,
+                ),
+              ),
+            ),
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+            inputFormatters: [CurrencyInputFormatter()],
+            style: GoogleFonts.manrope(
+              fontSize: 36,
+              color: Colors.green,
+              fontWeight: FontWeight.bold,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
